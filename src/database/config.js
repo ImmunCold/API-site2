19bbeeb935eef52f20bef47482c8fe0d9218c9e7
Andreas <andreas.bittencourt@sptech.school>
2022-04-27 16:03:42 -0300

    site atualizado

--- src/database/config.js
+++ src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "root",
-    database: "",
-    password: "",
+    user: "aluno",
+    database: "immuncold",
+    password: "sptech",
 };
 
 function executar(instrucao) {
