73e1f9e635828e4591153204ee33b0e2df302509
Andreas Rodrigues <andreas.bittencourt@sptech.school>
2022-04-27 20:52:38 -0300

    API FUNCIONANDO

--- src/database/config.js
+++ src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    user: "aluno",
+    user: "root",
     database: "immuncold",
-    password: "sptech",
+    password: "Andreas2014",
 };
 
 function executar(instrucao) {
